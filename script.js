e49d1c6142e64ab6bed9c182b83088b8eb92f7f4
Yugsolanki <yugsolanki15@gmail.com>
2021-04-23 22:06:57 +0530

    Color

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-/* var node = document.getElementById('my-node');
-
-/* domtoimage.toPng(node)
-    .then(function (dataUrl) {
-        var img = new Image();
-        img.src = dataUrl;
-        document.body.appendChild(img);
-    })
-    .catch(function (error) {
-        console.error('oops, something went wrong!', error);
-    });
-    
-  domtoimage.toBlob(document.getElementById('my-node'))
-    .then(function (blob) {
-        window.saveAs(blob, 'my-node.png');
-    });
-    
-domtoimage.toJpeg(document.getElementById('my-node'), { quality: 1 })
-    .then(function (dataUrl) {
-        var link = document.createElement('a');
-        link.download = 'my-image-name.jpeg';
-        link.href = dataUrl;
-        link.click();
-    });
-    
-    */
-    
 function downloadimage() {
             var container = document.getElementById("my-node"); //specific element on page
             
